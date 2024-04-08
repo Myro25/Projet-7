374a950f5b8a8dd71f0d20998a5f33558490ed24
Myro25 <132125266+Myro25@users.noreply.github.com>
2024-04-08 23:26:45 +0200

    a jour

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+/* Import des modules necessaires */
+const express = require("express");
+const router = express.Router();
+
+const userCtrl = require("../controllers/user.controller");
+
+const limiter = require("../middleware/GuardRateLimit");
+const GuardPasswordValidator = require("../middleware/GuardPasswordValidator");
+
+/* Routage User */
+router.post("/signup", GuardPasswordValidator, userCtrl.signup);
+router.post("/login", limiter, userCtrl.login);
+
+module.exports = router;
+
+
+
